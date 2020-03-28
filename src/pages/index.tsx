81efe2109f22f4f71b7cbb652934c82a2aa01eb2
kaave <junkjunctions@gmail.com>
2020-03-29 01:33:42 +0900

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const HomePage = () => (
+  <div>
+    <h1>Hello, World!</h1>
+    <Link href="/second-page">
+      <button type="button">Goto Second Page</button>
+    </Link>
+  </div>
+);
+
+export default HomePage;
