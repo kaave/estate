0224ccc6aba55d56d9f0361f422924666496717f
kaave <junkjunctions@gmail.com>
2020-04-04 22:59:05 +0900

    add: markdown-it

--- tools/loadPosts.js
+++ tools/loadPosts.js
@@ -1,5 +1,12 @@
 const contentful = require('contentful');
-const format = require('date-fns/format');
+const markdownIt = require('markdown-it');
+const prism = require('markdown-it-prism');
+const attrs = require('markdown-it-attrs');
+
+const md = markdownIt({ html: true, linkify: true });
+// md.use(prism, options);
+md.use(prism);
+md.use(attrs);
 
 async function loadPosts(spaceId, accessToken) {
   const client = contentful.createClient({
@@ -12,7 +19,14 @@ async function loadPosts(spaceId, accessToken) {
     order: '-fields.published',
   });
 
-  return entries;
+  return entries.items.map(({ sys, fields }) => ({
+    sys,
+    fields: {
+      ...fields,
+      rawPost: fields.post,
+      post: md.render(fields.post),
+    },
+  }));
 }
 
 module.exports = loadPosts;
--- tools/savePosts.js
+++ tools/savePosts.js
@@ -9,16 +9,13 @@ async function savePosts(entries) {
 
   await fs.mkdirp(savePath);
 
-  const writeJsonPromises = entries.items.map((item) => {
+  const writeJsonPromises = entries.map((item) => {
     const { id } = item.sys;
     const filePath = path.join(savePath, `${id}.json`);
     return fs.writeJson(filePath, item, { spaces: 4 });
   });
 
-  await Promise.all([
-    ...writeJsonPromises,
-    fs.writeJson(path.join(savePath, 'all.json'), entries.items, { spaces: 4 }),
-  ]);
+  await Promise.all([...writeJsonPromises, fs.writeJson(path.join(savePath, 'all.json'), entries, { spaces: 4 })]);
 }
 
 module.exports = savePosts;
