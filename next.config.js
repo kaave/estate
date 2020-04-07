5c21b395126f7f29f93c895026eebc14a8187b8c
kaave <junkjunctions@gmail.com>
2020-04-07 23:39:37 +0900

    fix: dynamic routes & css modules bug

--- next.config.js
+++ next.config.js
@@ -11,6 +11,31 @@ function webpack(config, { dev, isServer }) {
 
   config.resolve.plugins.push(new TsconfigPathsPlugin());
 
+  // @FIXME getLocalIdentとDynamic routesを併用すると不正なclass名となり動作しないため、
+  // 妥協してディレクトリ名を命名から排除する
+  // https://github.com/zeit/next.js/issues/10468
+  config.module.rules.forEach(
+    (rule) =>
+      rule.oneOf &&
+      rule.oneOf.forEach(
+        (rule) =>
+          rule.test instanceof RegExp &&
+          rule.use instanceof Array &&
+          rule.use
+            .filter(
+              (use) =>
+                typeof use.loader === 'string' &&
+                use.loader.includes('/css-loader') &&
+                !!use.options &&
+                !!use.options.modules,
+            )
+            .forEach((use) => {
+              const { getLocalIdent, ...rest } = use.options.modules;
+              use.options.modules = { ...rest, localIdentName: '[local]___[hash:base64:5]' };
+            }),
+      ),
+  );
+
   config.plugins = [
     ...config.plugins,
     new DotenvWebpack({
@@ -64,6 +89,10 @@ const nextOptions = {
   webpack,
   ...analyzerOptions,
   ...optimizedImagesOptions,
+  cssLoaderOptions: {
+    // importLoaders: 1,
+    localIdentName: '[local]___[hash:base64:5]',
+  },
   // ...workboxOptions,
   // exportPathMap: async (defaultPathMap) => {
   //   const posts = await fs.readJson(path.join(__dirname, 'public', 'static', 'posts', 'all.json'));
--- src/pages/posts/[id]/index.tsx
+++ src/pages/posts/[id]/index.tsx
@@ -19,23 +19,24 @@ const Posts = React.memo(({ post: { title, post, published, thumbnail, tags } = 
 
   return (
     <Layout appendTitles={[title, 'POSTS']} descriptionArgv={title} path={asPath}>
-      <h2 className={styles.header}>
-        {title}
-        <time className={styles.published} dateTime={datetime}>
-          {datetime}
-        </time>
-      </h2>
-      <img src={thumbnail.url} alt={thumbnail.title} />
-
-      {tags.length > 0 ? (
-        <ul>
-          {tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      ) : null}
-      {/* eslint-disable-next-line react/no-danger */}
-      <div className={styles.content} dangerouslySetInnerHTML={{ __html: post }} />
+      <article className={styles.root}>
+        <h2 className={styles.header}>
+          {title}
+          <time className={styles.published} dateTime={datetime}>
+            {datetime}
+          </time>
+        </h2>
+        <img src={thumbnail.url} alt={thumbnail.title} />
+        {tags.length > 0 ? (
+          <ul>
+            {tags.map((tag) => (
+              <li key={tag}>{tag}</li>
+            ))}
+          </ul>
+        ) : null}
+        {/* eslint-disable-next-line react/no-danger */}
+        <div className={styles.content} dangerouslySetInnerHTML={{ __html: post }} />
+      </article>
     </Layout>
   );
 });
