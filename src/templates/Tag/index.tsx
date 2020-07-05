dab3881d78836da03316bb72bac11b2aa56a3d3e
kaave <junkjunctions@gmail.com>
2020-07-05 18:04:29 +0900

    add: tag page

--- src/templates/Tag/index.tsx
+++ src/templates/Tag/index.tsx
@@ -1,9 +1,13 @@
-import React, { useMemo } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
+import { format } from 'date-fns';
 import Link from 'next/link';
-import format from 'date-fns/format';
 
 import type { Post } from '@domains/valueObjects/Post';
-import { Layout } from '@layouts/Default';
+import { Layout } from '@templates/layouts/Default';
+import { RouteHeader } from '@components/shared/RouteHeader';
+import { Label } from '@components/shared/Label';
+
+import styles from './index.module.scss';
 
 type Props = {
   tag: string;
@@ -11,29 +15,42 @@ type Props = {
   pathname: string;
 };
 
-export const TagTemplate = ({ tag, posts, pathname }: Props) => (
-  <Layout appendTitles={[tag, 'TAG']} descriptionArgv={tag} path={pathname}>
-    <section>
-      <h3>{tag}</h3>
-      <ul>
-        {posts.map(({ title, published }) => (
-          <Row key={published} title={title} published={published} />
-        ))}
-      </ul>
-    </section>
-  </Layout>
-);
+export const TagTemplate = ({ tag, posts, pathname }: Props) => {
+  const [hiddenHeader, setHiddenHeader] = useState(true);
+  const handleIntersectHeader = useCallback(() => setHiddenHeader(false), []);
+
+  return (
+    <Layout appendTitles={[tag, 'TAG']} descriptionArgv={tag} path={pathname}>
+      <div className={styles.inner}>
+        <RouteHeader lineCount={16} hidden={hiddenHeader} onIntersect={handleIntersectHeader}>
+          {`Tag: ${tag}`}
+        </RouteHeader>
+        <section className={styles.section}>
+          <ul className={styles.list}>
+            {posts.map(({ title, published }) => (
+              <Row key={published} title={title} published={published} />
+            ))}
+          </ul>
+        </section>
+      </div>
+    </Layout>
+  );
+};
 
 type RowProps = Pick<Post, 'title' | 'published'>;
 
 const Row = ({ title, published }: RowProps) => {
-  const datetime = useMemo(() => format(new Date(published), 'MMM, dd'), [published]);
+  const datetime = useMemo(() => format(new Date(published), 'MMM, dd yyyy'), [published]);
 
   return (
-    <li>
+    <li className={styles.row}>
       <Link href="/posts/[published]" as={`/posts/${published}`}>
-        <a>
-          {datetime} | {title}
+        <a className={styles.link}>
+          <time className={styles.date} dateTime={datetime}>
+            <Label>{datetime}</Label>
+          </time>
+          {title}
+          <span className={styles.linkLine} role="presentation" />
         </a>
       </Link>
     </li>
