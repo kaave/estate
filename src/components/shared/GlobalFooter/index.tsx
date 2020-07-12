8047af072e6f9f24a3aa68cb4f61cfe04b473ba3
kaave <junkjunctions@gmail.com>
2020-07-12 21:32:41 +0900

    fix: footer logo link

--- src/components/shared/GlobalFooter/index.tsx
+++ src/components/shared/GlobalFooter/index.tsx
@@ -9,28 +9,30 @@ type Props = {
   current?: typeof links[number];
 };
 
-export const GlobalFooter = ({ current }: Props) => {
-  return (
-    <footer className={styles.root}>
-      <div className={styles.inner}>
-        <h2 className={styles.header}>{configs.title}</h2>
-        <nav className={styles.nav}>
-          <ul className={styles.list}>
-            {links.map((link) => (
-              <li key={link} className={styles.cell}>
-                <Link href={`/${link}`}>
-                  <a className={styles.link} aria-current={current === link ? 'page' : undefined}>
-                    <span className={styles.linkText}>{link}</span>
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <p className={styles.copyright}>
-          <small className={styles.copyrightText}>© kaave</small>
-        </p>
-      </div>
-    </footer>
-  );
-};
+export const GlobalFooter = ({ current }: Props) => (
+  <footer className={styles.root}>
+    <div className={styles.inner}>
+      <h2 className={styles.header}>
+        <Link href="/">
+          <a className={styles.headerLink}>{configs.title}</a>
+        </Link>
+      </h2>
+      <nav className={styles.nav}>
+        <ul className={styles.list}>
+          {links.map((link) => (
+            <li key={link} className={styles.cell}>
+              <Link href={`/${link}`}>
+                <a className={styles.link} aria-current={current === link ? 'page' : undefined}>
+                  <span className={styles.linkText}>{link}</span>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <p className={styles.copyright}>
+        <small className={styles.copyrightText}>© kaave</small>
+      </p>
+    </div>
+  </footer>
+);
