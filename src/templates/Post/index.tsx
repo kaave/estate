fbee5584532648e4dd29d8eac75582e95fc59495
kaave <junkjunctions@gmail.com>
2020-07-12 21:26:06 +0900

    fix: post first view

--- src/templates/Post/index.tsx
+++ src/templates/Post/index.tsx
@@ -31,8 +31,13 @@ export const PostTemplate = ({ post: { title, post, published, thumbnail, tags }
   return (
     <Layout appendTitles={[title, 'POSTS']} descriptionArgv={title} path={pathname}>
       <article className={styles.root}>
-        <header ref={headingRef} className={styles.header} hidden={headingHidden || undefined}>
-          <img className={styles.thumbnail} src={thumbnail.url} alt={thumbnail.title} />
+        <header
+          ref={headingRef}
+          className={styles.header}
+          hidden={headingHidden || undefined}
+          style={{ backgroundImage: `url(${thumbnail.url})` }}
+        >
+          <img className={`${styles.thumbnail} -hidden-pc`} src={thumbnail.url} alt={thumbnail.title} />
           <h2 className={styles.heading}>
             <span className={styles.headingTitle}>
               <span className={styles.headingTitleText}>{title}</span>
