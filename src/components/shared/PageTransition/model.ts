6861b3942e8a7b009f4b1a158ba8984a1a1f9ac8
kaave <junkjunctions@gmail.com>
2020-07-23 00:09:21 +0900

    add: initialize state

--- src/components/shared/PageTransition/model.ts
+++
@@ -1,4 +0,0 @@
-import type { ReactNode } from 'react';
-
-export type Children = ReactNode;
-export type TransitionState = 'init' | 'enter' | 'entering' | 'entered' | 'exit' | 'exiting' | 'exited';
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
-import type { AppProps } from 'next/app';
+import React, { useEffect, useRef, useState } from 'react';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import Router from 'next/router';
 import FontFaceObserver from 'fontfaceobserver';
+import type { AppProps } from 'next/app';
 
 import { pageView } from '@utils/gtag';
+import { crawlerAccess } from '@utils/crawlers';
 import * as configs from '@utils/configs';
 
 import '@styles/index.scss';
-import { PageTransition } from '@components/shared/PageTransition';
 
 if (configs.googleAnalytics) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
@@ -15,19 +16,33 @@ if (configs.googleAnalytics) {
 }
 
 const App = ({ Component, pageProps, router }: AppProps) => {
+  const nodeRef = useRef(null);
+  const [fontInitialized, setFontInitialized] = useState(false);
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     (async () => {
       const ffo = new FontFaceObserver('Crimson Text');
-      const start = performance.now();
       await ffo.load();
-      console.log('loaded Crimson Text', performance.now() - start);
+      setFontInitialized(true);
     })();
   }, []);
+
+  const visible = crawlerAccess || !fontInitialized;
+
   return (
-    <PageTransition timeout={300} classNames="page-transition">
-      <Component key={router.asPath} {...pageProps} />
-    </PageTransition>
+    <SwitchTransition>
+      <CSSTransition
+        key={router.asPath}
+        nodeRef={nodeRef}
+        timeout={300}
+        classNames="page-transition"
+        // addEndListener={(c, cb) => console.log(c, cb)}
+      >
+        <div ref={nodeRef} className="wrapper" hidden={visible || undefined}>
+          <Component {...pageProps} />
+        </div>
+      </CSSTransition>
+    </SwitchTransition>
   );
 };
 
